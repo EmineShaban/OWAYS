4d2b4a24ed50505bfecbe8081f7f555f8a01b2ea
Emine Shaban <emineshaban@icloud.com>
2024-07-12 14:56:37 +0300

    ,

--- js/index.js
+++ js/index.js
@@ -13,3 +13,20 @@ let easyImg = document.getElementsByClassName("easy-img");
 
     })
  }
+
+
+ 
+let q = document.getElementsByClassName("q");
+let a = document.getElementsByClassName("a");
+const onOpen = (e) => {
+  if (e.target.classList == "q") {
+    console.log(e.target.parentElement.children[1]);
+    for (let i = 0; i < a.length; i++) {
+      a[i].style.display = "none";
+    }
+    e.target.parentElement.children[1].style.display = "block";
+  }
+};
+for (let index = 0; index < q.length; index++) {
+  q[index].addEventListener("click", onOpen);
+}
